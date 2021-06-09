71fc6dcc2389056abf3e0f0dbb46a630a297b1e6
datawhales <datawhales@gmail.com>
2021-06-10 02:54:10 +0900

    modified binary search

--- src/binary_search.py
+++ src/binary_search.py
@@ -14,6 +14,21 @@ def binary_search(L, trg):
     else:
         return -1
 
+def binary_search_2(L, trg, start, end):
+    """ Recursion을 이용한 이진 탐색 함수.
+        하지만 이 경우는 구하려는 값이 처음으로 나타나는 인덱스가 아니라,
+        존재하는 인덱스 중의 하나를 반환.
+    """
+    if start > end:
+        return
+    mid = (start + end) // 2
+    if L[mid] == trg:
+        return mid
+    elif L[mid] < trg:
+        return binary_search_2(L, trg, mid+1, end)
+    else:
+        return binary_search_2(L, trg, start, mid-1)
+
 from bisect import bisect_left, bisect_right
 
 def count_by_range(L, left_value, right_value):
@@ -34,9 +49,10 @@ def count_by_range(L, left_value, right_value):
 
 if __name__ == "__main__":
     L = [-7, -6, -2, 0, 3, 4, 4, 5, 7, 9, 50, 100]
-    trg = 9
+    trg = 4
     # 리스트 L에서 trg가 첫 번째로 나타나는 인덱스를 찾음
     print(binary_search(L, trg))
+    print(binary_search_2(L, trg, 0, len(L)-1))
     # 리스트 L에 trg를 삽입하려 할 때 가장 왼쪽 인덱스를 찾음
     print(bisect_left(L, trg))
     # 리스트 L에 trg를 삽입하려 할 때 가장 오른쪽 인덱스를 찾음
